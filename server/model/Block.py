e48b15b84ff13612b4fac4e91ed3f5b6fc3fe8ad
otboss <otsurfer6@gmail.com>
2021-11-09 23:42:11 -0500

    implemented block and record classes

--- server/model/Block.py
+++ server/model/Block.py
@@ -1,3 +1,13 @@
 
+from typing import List
+from Record import  Record
+
+
 class Block:
-  pass
+  records: List[List] = []
+
+  def add_record(self, record: Record):
+    if(len(self.records) >= 5):
+      raise Exception("block full") 
+      
+    self.records.append(record.to_list())

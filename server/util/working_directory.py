e4a97acb34c3e154c12a82734a2e8f59fba3a036
otboss <otsurfer6@gmail.com>
2021-11-16 00:34:40 -0500

    corrected working directory module

---
+++ server/util/working_directory.py
@@ -0,0 +1,25 @@
+import os 
+import sys
+
+db_path_file = "./db_path.txt"
+
+def create_working_directory():
+    working_directory_path: str = "./%s-db"%(sys.argv[1]) if len(sys.argv) > 1 else "./default-db"
+    try:
+        os.mkdir(working_directory_path)
+    except:
+        pass
+    file = open(db_path_file, "w")
+    file.write(working_directory_path)
+    file.close()
+        
+def load_working_directory():
+    file = open(db_path_file, "r")
+    working_dir_path = file.read()
+    file.close()
+    return working_dir_path
+
+def create_sub_folder(folder_name):
+    sub_path = load_working_directory() + "/" + folder_name
+    os.mkdir(sub_path)
+       

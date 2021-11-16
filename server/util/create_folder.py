7637c268fc190b5ca88748db575399df8e6f6dd4
Denisha Armstrong <denisha.armstrong@ads.taj.gov.jm>
2021-11-15 23:26:52 -0500

    Update create_folder

--- server/util/create_folder.py
+++ server/util/create_folder.py
@@ -4,31 +4,13 @@ global workingDir
 baseLocation = 'server/'
 
 def createWorkingDir(workingDir):
-    isFile = os.path.isdir(workingDir)
-    if isFile is False:
-        print('Path not found!!!')
-        question = input('Create Path [yes or no] : ')    
-        if question.lower() == 'yes' or question.lower() == 'y':
-            os.mkdir(baseLocation+workingDir)
-            file = open("workingDirectory.txt", "w")
-            file.write(workingDir)
-            file.close()
-            print('Path Successfully Created ', workingDir)
+    os.mkdir(baseLocation+workingDir)
+    file = open("workingDirectory.txt", "w")
+    file.write(workingDir)
+    file.close()
 
-        else:
-            print('Exiting Program.........')
-            exit()
-
+        
 def createsubFolder(folderName):
-    if folderName:
-        subPath = workingDir + "\\" + folderName
-        os.mkdir(subPath)
-        print('Path Successfully Created. ', subPath)
-
-
-workingDir = input('Please enter Working Directory: ')
-createWorkingDir(workingDir)
-
-fName = "DB_Folder"
-createsubFolder(fName)
-
+    subPath = workingDir + "\\" + folderName
+    os.mkdir(subPath)
+       
